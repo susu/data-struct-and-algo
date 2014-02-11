45771ad450b5d622939820960b71a3f4ef52ead1
Marton Suranyi <marton.suranyi@gmail.com>
2014-02-12 00:10:27 +0100

    Stack implemented

--- include/ds/Stack.hpp
+++ include/ds/Stack.hpp
@@ -1,17 +1,54 @@
 #ifndef DS_STACK_HPP_INC
 #define DS_STACK_HPP_INC
 
+#include <memory>
+#include <cstring>
+
 namespace ds
 {
   template<typename T>
   class Stack
   {
     public:
+      const static size_t INITIAL_CAPACITY = 3;
       Stack()
+        : m_data(new T[INITIAL_CAPACITY])
+        , m_capacity(INITIAL_CAPACITY)
+        , m_size(0)
       {}
 
       bool isEmpty() const
       { return true; }
+
+      void push(const T& item)
+      {
+        ensureCapacity(m_size + 1);
+        m_data[m_size] = item;
+        ++m_size;
+      }
+
+      T pop()
+      {
+        assert(m_size > 0);
+        return m_data[--m_size];
+      }
+    private:
+      void ensureCapacity(size_t requiredCapacity)
+      {
+        if (m_capacity < requiredCapacity)
+        {
+          std::unique_ptr<T[]> newData(new T[m_capacity * 2]);
+          std::memcpy(newData.get(), m_data.get(), m_capacity * sizeof(T));
+
+          m_data.reset(newData.release());
+          m_capacity *= 2;
+        }
+      }
+
+    private:
+      std::unique_ptr<T[]> m_data;
+      size_t m_capacity;
+      size_t m_size;
   };
 }
 
--- test/Stack.cpp
+++ test/Stack.cpp
@@ -1,33 +1,83 @@
 #include <igloo/igloo_alt.h>
 #include <ds/Stack.hpp>
 
-using namespace igloo;
+#include <random>
 
-Describe(AStack)
+namespace
 {
-  It(should_be_instantiated)
+  using namespace igloo;
+
+  std::vector<int> generateRandomVector(int numberOfElements)
   {
-    ds::Stack<int> stack;
+    std::vector<int> ret(numberOfElements);
+    std::random_device dev;
+    std::uniform_int_distribution<> distribution(0, 1000);
+    for (auto & i : ret)
+    {
+      i = distribution(dev);
+    }
+    return std::move(ret);
   }
 
-  It(should_be_empty_on_creation)
+  Describe(AStack)
   {
-    // Arrange
-    ds::Stack<int> stack;
+    It(should_be_instantiated)
+    {
+      ds::Stack<int> stack;
+    }
 
-    // Act && Assert
-    AssertThat(stack.isEmpty(), Equals(true));
-  }
+    It(should_be_empty_on_creation)
+    {
+      // Arrange
+      ds::Stack<int> stack;
 
-  It(should_return_the_pushed_element)
-  {
-    // Arrange
-    ds::Stack<int> stack;
+      // Act && Assert
+      AssertThat(stack.isEmpty(), Equals(true));
+    }
 
-    // Act
-    stack.push(42);
+    It(should_return_the_pushed_element)
+    {
+      // Arrange
+      ds::Stack<int> stack;
+      stack.push(42);
 
-    // Assert
-    AssertThat(stack.pop(), 42);
-  }
-};
+      // Act
+      int poppedItem = stack.pop();
+
+      // Assert
+      AssertThat(poppedItem, Equals(42));
+    }
+
+    It(should_return_multiple_pushed_elements_in_reversed_order)
+    {
+      // Arrange
+      ds::Stack<int> stack;
+      stack.push(42);
+      stack.push(76);
+      stack.push(4);
+
+      // Act && Assert
+      AssertThat(stack.pop(), Equals(4));
+      AssertThat(stack.pop(), Equals(76));
+      AssertThat(stack.pop(), Equals(42));
+      AssertThat(stack.isEmpty(), Equals(true));
+    }
+
+    It(should_return_lot_of_pushed_elements_in_reversed_order)
+    {
+      // Arrange
+      ds::Stack<int> stack;
+      std::vector<int> numbers = generateRandomVector(100);
+
+      for (const auto & i : numbers)
+        stack.push(i);
+
+      // Act && Assert
+      std::for_each(numbers.rbegin(), numbers.rend(),
+      [&stack](int i)
+      {
+        AssertThat(stack.pop(), Equals(i));
+      });
+    }
+  };
+}
